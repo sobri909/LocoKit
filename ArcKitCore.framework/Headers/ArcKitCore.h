b8aa9cc5f03865620c74be5d9c265950105652ba
Matt Greenfield <matt@bigpaua.com>
2017-11-17 23:36:20 +0700

    prep work for open sourcing some of the public interface classes

---
+++ ArcKitCore.framework/Headers/ArcKitCore.h
@@ -0,0 +1,19 @@
+//
+//  ArcKit.h
+//  ArcKit
+//
+//  Created by Matt Greenfield on 2/07/17.
+//  Copyright © 2017 Big Paua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArcKit.
+FOUNDATION_EXPORT double ArcKitVersionNumber;
+
+//! Project version string for ArcKit.
+FOUNDATION_EXPORT const unsigned char ArcKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcKit/PublicHeader.h>
+
+
