0ba64ac6257d32da1877b1b2661811f57c84fc58
Matt Greenfield <matt@bigpaua.com>
2018-02-05 20:02:45 +0700

    disable Crashlytics

--- ArcKit Demo App/AppDelegate.swift
+++ ArcKit Demo App/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        Fabric.with([Crashlytics.self])
+//        Fabric.with([Crashlytics.self])
 
         window = UIWindow(frame: UIScreen.main.bounds)
         
