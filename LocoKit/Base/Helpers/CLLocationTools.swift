7c346287336d11f895a01f0ea378ab4050c3ef7d
Matt Greenfield <matt@bigpaua.com>
2018-08-09 21:25:04 +0700

    Codable for CLLocationCoordinate2D

--- LocoKit/Base/Helpers/CLLocationTools.swift
+++ LocoKit/Base/Helpers/CLLocationTools.swift
@@ -98,6 +98,23 @@ public extension CLLocationCoordinate2D {
     }
 }
 
+extension CLLocationCoordinate2D: Codable {
+    enum CodingKeys: String, CodingKey {
+        case latitude, longitude
+    }
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+        let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+        self.init(latitude: latitude, longitude: longitude)
+    }
+}
+
 // MARK: - CLLocation
 
 public extension CLLocation {
