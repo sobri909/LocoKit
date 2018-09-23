c977c25148eea02ac642e82b12bc7569eec75eba
Matt Greenfield <matt@bigpaua.com>
2018-09-23 23:00:43 +0700

    this version number is useful outside of LocoKit

--- LocoKit/Timelines/ActivityTypes/ActivityType.swift
+++ LocoKit/Timelines/ActivityTypes/ActivityType.swift
@@ -10,7 +10,7 @@ import GRDB
 
 open class ActivityType: MLModel, PersistableRecord {
 
-    static let currentVersion = 700000
+    public static let currentVersion = 700000
 
     static let numberOfLatBucketsDepth0 = 18
     static let numberOfLongBucketsDepth0 = 36
