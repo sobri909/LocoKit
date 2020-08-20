9e9bffe2b7677627275a405ae9e0e287127c14c3
Matt Greenfield <matt@bigpaua.com>
2020-08-20 18:49:41 +0700

    back to faster processing loops
    Signed-off-by: Matt Greenfield <matt@bigpaua.com>

--- LocoKit/Timelines/TimelineProcessor.swift
+++ LocoKit/Timelines/TimelineProcessor.swift
@@ -46,7 +46,7 @@ public class TimelineProcessor {
             // recurse until no remaining possible merges
             process(items) { results in
                 if let kept = results?.kept {
-                    delay(1) { process(from: kept) }
+                    delay(0.3) { process(from: kept) }
                 }
             }
         }
