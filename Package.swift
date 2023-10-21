23d32052aa690089a78f825e5831786cd1a9037c
Matt <matt@bigpaua.com>
2023-10-21 10:49:08 +0700

    require iOS 16, to simplify some stuff

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LocoKit",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(name: "LocoKit", targets: ["LocoKit"])
     ],
