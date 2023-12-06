bdee460719453d029536b5b945036f179f125430
Matt <matt@bigpaua.com>
2023-12-06 15:52:24 +0700

    experimenting with query planner hinting, that probably doesn’t matter now that i’ve removed all possible indexes except one

--- LocoKit/Timelines/ActivityTypes/Core ML/CoreMLModelWrapper.swift
+++ LocoKit/Timelines/ActivityTypes/Core ML/CoreMLModelWrapper.swift
@@ -316,10 +316,10 @@ public class CoreMLModelWrapper: DiscreteClassifier, PersistableRecord, Hashable
         return store.samples(
             inside: rect,
             where: """
-                    confirmedType IS NOT NULL
-                    AND xyAcceleration IS NOT NULL
-                    AND zAcceleration IS NOT NULL
-                    AND stepHz IS NOT NULL
+                    unlikely(confirmedType IS NOT NULL)
+                    AND likely(xyAcceleration IS NOT NULL)
+                    AND likely(zAcceleration IS NOT NULL)
+                    AND likely(stepHz IS NOT NULL)
                     ORDER BY lastSaved DESC
                     LIMIT ?
                 """,
