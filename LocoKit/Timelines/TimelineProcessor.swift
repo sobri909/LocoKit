8123f4201e5a2989825dc9b674c083e9fe68f674
Matt <matt@bigpaua.com>
2024-07-28 09:45:36 +0800

    this sometimes mysteriously runs very long / freezes

--- LocoKit/Timelines/TimelineProcessor.swift
+++ LocoKit/Timelines/TimelineProcessor.swift
@@ -933,7 +933,7 @@ public class TimelineProcessor {
                     WHERE timelineItemId = TimelineItem.itemId
                     AND LocomotionSample.disabled = TimelineItem.disabled
                 )
-            WHERE deleted = 0 AND disabled = 0 AND startDate IS NULL
+            WHERE deleted = 0 AND disabled = 0 AND startDate IS NULL LIMIT 100
         """
 
         do {
